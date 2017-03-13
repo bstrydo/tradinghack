fc933faf5590033cc46bb072aa2b717bf203f401
stefanlengton <slengton@thoughtworks.com>
2017-03-13 16:47:57 +0200

    Add solution with backtest project files

---
+++ src/BackTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

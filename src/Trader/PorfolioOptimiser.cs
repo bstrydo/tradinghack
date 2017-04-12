0db2629f99a11bdf9f0a7176d58ccbbd374fc117
stefanlengton <slengton@thoughtworks.com>
2017-04-12 10:42:09 +0200

    Change portfolio optimiser to handle incorrectly ordered lists

--- src/Trader/PorfolioOptimiser.cs
+++ src/Trader/PorfolioOptimiser.cs
@@ -18,11 +18,20 @@ namespace Trader
 
         public void Optimise()
         {
-            double totalChange = historicPrices.Zip(forecastPrices, (h, f) => f.Price / h.Price - 1).Sum();
-            portfolio = historicPrices.Zip(forecastPrices, (h, f) => new Tuple<string, double>(h.Symbol, (f.Price / h.Price - 1) / totalChange))
+            var orderedHistoricPrices = historicPrices.OrderBy(p => p.Symbol);
+            var orderedForecastPrices = forecastPrices.OrderBy(p => p.Symbol);
+            double totalChange = orderedHistoricPrices.Zip(orderedForecastPrices, (h, f) => f.Price / h.Price - 1).Sum();
+            portfolio = orderedHistoricPrices.Zip(orderedForecastPrices, (h, f) => new Tuple<string, double>(h.Symbol, (f.Price / h.Price - 1) / totalChange))
                 .ToDictionary(x => x.Item1, x => x.Item2);
         }
 
-        public Dictionary<string, double> Profile { get { return portfolio; } }
+        public Dictionary<string, double> Portfolio
+        {
+            get
+            {
+                if (portfolio == null) throw new InvalidOperationException("Can't retrieve portfolio before optimising");
+                return portfolio;
+            }
+        }
     }
 }
--- test/Trader.Tests/PortfolioOptimiserTests.cs
+++ test/Trader.Tests/PortfolioOptimiserTests.cs
@@ -1,5 +1,6 @@
-using Xunit;
+using System;
 using System.Collections.Generic;
+using Xunit;
 
 namespace Trader
 {
@@ -8,7 +9,7 @@ namespace Trader
         [Theory]
         [InlineData("AAPL", 143.17, 145.23)]
         [InlineData("GOOGL", 824.17, 824.73)]
-        public void Optimise_StockGoingUp_BuyStock(string symbol, double historicPrice, double futurePrice)
+        public void Optimise_StockGoingUp_LongStock(string symbol, double historicPrice, double futurePrice)
         {
             List<HistoricPrice> historicPrices = new List<HistoricPrice>();
             historicPrices.Add(new HistoricPrice(symbol, historicPrice));
@@ -18,11 +19,11 @@ namespace Trader
 
             portfolioOptimiser.Optimise();
 
-            Assert.Equal(portfolioOptimiser.Profile, new Dictionary<string, double>() { { symbol, 1 } });
+            Assert.Equal(portfolioOptimiser.Portfolio, new Dictionary<string, double>() { { symbol, 1 } });
         }
 
         [Fact]
-        public void Optimise_Stocks_GoingUp_LongStocks()
+        public void Optimise_StocksGoingUp_LongStocks()
         {
             List<HistoricPrice> historicPrices = new List<HistoricPrice>();
             historicPrices.Add(new HistoricPrice("AAPL", 143.17));
@@ -37,7 +38,40 @@ namespace Trader
             var changeInApple = (146.03 / 143.17 - 1);
             var changeInGoogle = (840.65 / 824.17 - 1);
             var totalChange = changeInApple + changeInGoogle;
-            Assert.Equal(portfolioOptimiser.Profile, new Dictionary<string, double>() { { "AAPL", changeInApple / totalChange }, { "GOOGL", changeInGoogle / totalChange } });
+            Assert.Equal(portfolioOptimiser.Portfolio, new Dictionary<string, double>() { { "AAPL", changeInApple / totalChange }, { "GOOGL", changeInGoogle / totalChange } });
+        }
+
+        [Fact]
+        public void Portfolio_RetrieveBeforeOptimise_ShouldThrowInvalidOperationException()
+        {
+
+            List<HistoricPrice> historicPrices = new List<HistoricPrice>();
+            historicPrices.Add(new HistoricPrice("AAPL", 143.17));
+            List<ForecastPrice> forecastPrices = new List<ForecastPrice>();
+            forecastPrices.Add(new ForecastPrice("AAPL", 146.03));
+            PortfolioOptimiser portfolioOptimiser = new PortfolioOptimiser(historicPrices, forecastPrices);
+
+            var exception = Assert.Throws<InvalidOperationException>(() => portfolioOptimiser.Portfolio);
+            Assert.Equal("Can't retrieve portfolio before optimising", exception.Message);
+        }
+
+        [Fact]
+        public void Optimise_StocksGoingUpAnyOrder_LongStocks()
+        {
+            List<HistoricPrice> historicPrices = new List<HistoricPrice>();
+            historicPrices.Add(new HistoricPrice("GOOGL", 824.17));
+            historicPrices.Add(new HistoricPrice("AAPL", 143.17));
+            List<ForecastPrice> forecastPrices = new List<ForecastPrice>();
+            forecastPrices.Add(new ForecastPrice("AAPL", 146.03));
+            forecastPrices.Add(new ForecastPrice("GOOGL", 840.65));
+            PortfolioOptimiser portfolioOptimiser = new PortfolioOptimiser(historicPrices, forecastPrices);
+
+            portfolioOptimiser.Optimise();
+
+            var changeInApple = (146.03 / 143.17 - 1);
+            var changeInGoogle = (840.65 / 824.17 - 1);
+            var totalChange = changeInApple + changeInGoogle;
+            Assert.Equal(portfolioOptimiser.Portfolio, new Dictionary<string, double>() { { "AAPL", changeInApple / totalChange }, { "GOOGL", changeInGoogle / totalChange } });
         }
     }
 }
